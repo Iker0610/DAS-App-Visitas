a7a17a2003c541bcc13d9064f19afc37c48913c9
iker0610 <ikerprogrammer0610@gmail.com>
2022-03-21 13:09:49 +0100

    Fixed visual BUG where expanded icon moved/disappeared when Client's name or address was too long

--- app/src/main/java/das/omegaterapia/visits/activities/main/composables/VisitItem.kt
+++ app/src/main/java/das/omegaterapia/visits/activities/main/composables/VisitItem.kt
@@ -6,16 +6,22 @@ import android.net.Uri
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.Card
 import androidx.compose.material.ContentAlpha
@@ -37,6 +43,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -104,41 +111,35 @@ fun VisitCardItem(
     // UI
     Card(modifier = modifier.fillMaxWidth(), elevation = elevation) {
         Column {
-            CenteredRow(
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier
                     .fillMaxWidth()
                     .clickable { onClick(visitCard) }
-                    .padding(16.dp),
-                horizontalArrangement = Arrangement.SpaceBetween
+                    .padding(16.dp)
             ) {
-                CenteredRow(horizontalArrangement = Arrangement.Start) {
-                    CenteredColumn {
-                        if (visitCard.isVIP) {
-                            Icon(Icons.Filled.StarOutline, "VIP Client", Modifier.height(16.dp), tint = MaterialTheme.colors.secondary)
-                            Spacer(modifier = Modifier.height(3.dp))
-                        }
-                        Text(text = date, style = typography.overline)
-                        if (!visitCard.isVIP) Spacer(modifier = Modifier.height(6.dp))
-                        Text(text = time, style = typography.subtitle2)
+                CenteredColumn {
+                    if (visitCard.isVIP) {
+                        Icon(Icons.Filled.StarOutline, "VIP Client", Modifier.height(16.dp), tint = MaterialTheme.colors.secondary)
+                        Spacer(modifier = Modifier.height(3.dp))
                     }
+                    Text(text = date, style = typography.overline)
+                    if (!visitCard.isVIP) Spacer(modifier = Modifier.height(6.dp))
+                    Text(text = time, style = typography.subtitle2)
+                }
 
-                    Spacer(modifier = Modifier.width(16.dp))
+                Spacer(modifier = Modifier.width(16.dp))
 
-                    Column(Modifier.wrapContentHeight()) {
-                        directionText()
-                        Spacer(modifier = Modifier.height(4.dp))
-                        mainText()
-                    }
+                Column(Modifier.weight(1f)) {
+                    directionText()
+                    Spacer(modifier = Modifier.height(4.dp))
+                    mainText()
                 }
 
                 if (canBeExpanded) {
-                    Icon(
-                        expandCollapseIcon,
-                        contentDescription = "Show more",
-                        Modifier
-                            .requiredSize(24.dp)
-                            .defaultMinSize(24.dp)
-                    )
+                    Box(contentAlignment = Alignment.CenterEnd, modifier = Modifier.requiredWidth(IntrinsicSize.Max).padding(start = 16.dp)) {
+                        Icon(expandCollapseIcon, contentDescription = "Show more", Modifier.size(24.dp))
+                    }
                 }
             }
 
@@ -234,7 +235,7 @@ fun VisitCardItemPreview() {
 fun VisitCardItemExpandedPreview() {
     OmegaterapiaTheme {
         Surface(color = Color.Black) {
-            VisitCardItem(modifier = Modifier.padding(8.dp), visitCard = visitList[0], isExpanded = true)
+            VisitCardItem(modifier = Modifier.padding(8.dp), visitCard = visitList[7], isExpanded = true)
         }
     }
 }

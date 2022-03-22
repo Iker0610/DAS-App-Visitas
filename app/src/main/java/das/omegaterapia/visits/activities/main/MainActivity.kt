20c3711eb2c30906fa27dff1967ffad79fafcea6
Iker de la Iglesia <ikerprogrammer0610@gmail.com>
2022-03-22 19:25:47 +0100

    Navigation Drawer Initial Implementation

--- app/src/main/java/das/omegaterapia/visits/activities/main/MainActivity.kt
+++ app/src/main/java/das/omegaterapia/visits/activities/main/MainActivity.kt
@@ -4,23 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.*
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
-import androidx.compose.animation.scaleIn
-import androidx.compose.animation.scaleOut
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.FabPosition
-import androidx.compose.material.Scaffold
-import androidx.compose.material.rememberScaffoldState
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.accompanist.navigation.animation.AnimatedNavHost
 import com.google.accompanist.navigation.animation.composable
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
@@ -30,8 +32,9 @@ import das.omegaterapia.visits.activities.main.composables.MainFloatingActionBut
 import das.omegaterapia.visits.activities.main.screens.AddVisitScreen
 import das.omegaterapia.visits.activities.main.screens.AllVisitsScreen
 import das.omegaterapia.visits.activities.main.screens.MainActivityScreens
+import das.omegaterapia.visits.activities.main.screens.navigableScreens
 import das.omegaterapia.visits.ui.theme.OmegaterapiaTheme
-import das.omegaterapia.visits.utils.isScrollingUp
+import das.omegaterapia.visits.utils.WindowSize
 import das.omegaterapia.visits.utils.rememberWindowSizeClass
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -45,86 +48,142 @@ class MainActivity : ComponentActivity() {
     /*--------------------------------------------------
     |            Activity Lifecycle Methods            |
     --------------------------------------------------*/
-    @OptIn(ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             OmegaterapiaTheme {
                 val windowSize = rememberWindowSizeClass()
+                MainActivityScreen(visitViewModel, windowSize = windowSize)
+            }
+        }
+    }
+}
 
-                val navController = rememberAnimatedNavController()
-                val currentRoute by navController.currentBackStackEntryFlow.collectAsState(initial = navController.currentBackStackEntry)
+@OptIn(ExperimentalAnimationApi::class, ExperimentalMaterialApi::class)
+@Composable
+private fun MainActivityScreen(
+    visitViewModel: VisitsViewModel = viewModel(),
+    windowSize: WindowSize
+) {
+    val scope = rememberCoroutineScope()
 
-                val scaffoldState = rememberScaffoldState()
-                val allVisitsLazyListState = rememberLazyListState()
+    val navController = rememberAnimatedNavController()
+    val currentRoute by navController.currentBackStackEntryFlow.collectAsState(initial = navController.currentBackStackEntry)
 
-                val scope = rememberCoroutineScope()
+    val scaffoldState = rememberScaffoldState()
+    val allVisitsLazyListState = rememberLazyListState()
 
-                Scaffold(
-                    scaffoldState = scaffoldState,
-                    floatingActionButton = {
-                        AnimatedVisibility(
-                            currentRoute?.destination?.route != MainActivityScreens.AddVisit.route && !allVisitsLazyListState.isScrollInProgress,
-                            enter = scaleIn(),
-                            exit = scaleOut()
-                        ) {
-                            MainFloatingActionButton(
-                                onClick = { navController.navigate(MainActivityScreens.AddVisit.route) },
-                            )
-                        }
-                    },
-                    floatingActionButtonPosition = FabPosition.Center,
-                    isFloatingActionButtonDocked = true,
-                    bottomBar = {
-                        AnimatedVisibility(
-                            currentRoute?.destination?.route != MainActivityScreens.AddVisit.route && !allVisitsLazyListState.isScrollInProgress,
-                            enter = slideInVertically(initialOffsetY = { 2 * it }),
-                            exit = slideOutVertically(targetOffsetY = { 2 * it })
-                        ) {
-                            BottomNavBar()
-                        }
-                    }
-                )
-                { innerPadding ->
-                    AnimatedNavHost(
-                        navController = navController,
-                        startDestination = MainActivityScreens.AllVisits.route
+    val drawerState = rememberBottomDrawerState(BottomDrawerValue.Closed)
+    val gesturesEnabled = drawerState.currentValue != BottomDrawerValue.Closed
+
+    BottomDrawer(
+        gesturesEnabled = gesturesEnabled,
+        drawerState = drawerState,
+        drawerShape = CutCornerShape(5),
+        drawerContent = {
+
+            LazyColumn(Modifier.padding(vertical = 24.dp)) {
+                item {
+                    Row(
+                        Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 32.dp),
+                        verticalAlignment = Alignment.CenterVertically
                     ) {
-                        composable(route = MainActivityScreens.AllVisits.route) {
-                            AllVisitsScreen(
-                                visitViewModel = visitViewModel,
-                                windowSize = windowSize,
-                                lazyListState = allVisitsLazyListState,
-                            )
-                        }
-                        composable(
-                            route = MainActivityScreens.AddVisit.route,
-                            enterTransition = {
-                                slideInVertically(
-                                    initialOffsetY = { 2 * it },
-                                    animationSpec = spring(dampingRatio = Spring.DampingRatioNoBouncy, stiffness = Spring.StiffnessVeryLow)
-                                )
-                            },
-                            exitTransition = {
-                                slideOutVertically(
-                                    targetOffsetY = { 2 * it },
-                                    animationSpec = spring(dampingRatio = Spring.DampingRatioNoBouncy, stiffness = Spring.StiffnessVeryLow)
-                                )
-                            },
-                        ) {
-                            AddVisitScreen(
-                                modifier = Modifier.padding(innerPadding),
-                                addVisitCard = visitViewModel::addVisitCard,
-                                onSuccessfulSubmit = {
-                                    scope.launch(Dispatchers.Main) {
-                                        if (!navController.popBackStack()) {
-                                            navController.navigate(MainActivityScreens.AllVisits.route)
-                                        }
-                                    }
-                                }
-                            )
-                        }
+                        Text(visitViewModel.currentUser, style = MaterialTheme.typography.h6)
                     }
+                    Divider(
+                        Modifier
+                            .padding(top = 22.dp, bottom = 8.dp)
+                            .fillMaxWidth()
+                    )
+                }
+                items(navigableScreens) {
+                    ListItem(
+                        modifier = Modifier.padding(horizontal = 16.dp),
+                        text = { Text(it.title) },
+                        icon = {
+                            Icon(
+                                it.icon,
+                                contentDescription = "Navigate to " + it.title
+                            )
+                        }
+                    )
+                }
+            }
+        },
+    ) {
+
+
+        Scaffold(
+            scaffoldState = scaffoldState,
+            floatingActionButton = {
+                AnimatedVisibility(
+                    currentRoute?.destination?.route != MainActivityScreens.AddVisit.route && !allVisitsLazyListState.isScrollInProgress,
+                    enter = scaleIn(),
+                    exit = scaleOut()
+                ) {
+                    MainFloatingActionButton(
+                        onClick = { navController.navigate(MainActivityScreens.AddVisit.route) },
+                    )
+                }
+            },
+            floatingActionButtonPosition = FabPosition.Center,
+            isFloatingActionButtonDocked = true,
+            bottomBar = {
+                AnimatedVisibility(
+                    currentRoute?.destination?.route != MainActivityScreens.AddVisit.route && !allVisitsLazyListState.isScrollInProgress,
+                    enter = slideInVertically(initialOffsetY = { 2 * it }),
+                    exit = slideOutVertically(targetOffsetY = { 2 * it })
+                ) {
+                    BottomNavBar(onMenuOpen = { scope.launch { drawerState.open() } })
+                }
+            }
+        )
+        { innerPadding ->
+            AnimatedNavHost(
+                navController = navController,
+                startDestination = MainActivityScreens.AllVisits.route
+            ) {
+                composable(route = MainActivityScreens.AllVisits.route) {
+                    AllVisitsScreen(
+                        visitViewModel = visitViewModel,
+                        windowSize = windowSize,
+                        lazyListState = allVisitsLazyListState,
+                    )
+                }
+                composable(
+                    route = MainActivityScreens.AddVisit.route,
+                    enterTransition = {
+                        slideInVertically(
+                            initialOffsetY = { 2 * it },
+                            animationSpec = spring(
+                                dampingRatio = Spring.DampingRatioNoBouncy,
+                                stiffness = Spring.StiffnessVeryLow
+                            )
+                        )
+                    },
+                    exitTransition = {
+                        slideOutVertically(
+                            targetOffsetY = { 2 * it },
+                            animationSpec = spring(
+                                dampingRatio = Spring.DampingRatioNoBouncy,
+                                stiffness = Spring.StiffnessVeryLow
+                            )
+                        )
+                    },
+                ) {
+                    AddVisitScreen(
+                        modifier = Modifier.padding(innerPadding),
+                        addVisitCard = visitViewModel::addVisitCard,
+                        onSuccessfulSubmit = {
+                            scope.launch(Dispatchers.Main) {
+                                if (!navController.popBackStack()) {
+                                    navController.navigate(MainActivityScreens.AllVisits.route)
+                                }
+                            }
+                        }
+                    )
                 }
             }
         }
--- app/src/main/java/das/omegaterapia/visits/activities/main/composables/NavigationComposables.kt
+++ app/src/main/java/das/omegaterapia/visits/activities/main/composables/NavigationComposables.kt
@@ -1,44 +1,22 @@
 package das.omegaterapia.visits.activities.main.composables
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.selection.toggleable
 import androidx.compose.foundation.shape.CutCornerShape
-import androidx.compose.material.BottomAppBar
-import androidx.compose.material.BottomDrawer
-import androidx.compose.material.BottomDrawerValue
-import androidx.compose.material.Button
-import androidx.compose.material.Checkbox
-import androidx.compose.material.ContentAlpha
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.ListItem
-import androidx.compose.material.LocalContentAlpha
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.rememberBottomDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import das.omegaterapia.visits.R
-import kotlinx.coroutines.launch
 
 private val FABShape = CutCornerShape(50)
 
@@ -51,18 +29,21 @@ fun MainFloatingActionButton(
             modifier = Modifier.padding(horizontal = 16.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Icon(imageVector = Icons.Default.Add, contentDescription = stringResource(R.string.fab_icon_description))
+            Icon(
+                imageVector = Icons.Default.Add,
+                contentDescription = stringResource(R.string.fab_icon_description)
+            )
         }
     }
 }
 
 @Composable
-fun BottomNavBar() {
+fun BottomNavBar(onMenuOpen: () -> Unit) {
 
     BottomAppBar(cutoutShape = FABShape) {
         // Leading icons should typically have a high content alpha
         CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.high) {
-            IconButton(onClick = { /* doSomething() */ }) {
+            IconButton(onClick = onMenuOpen) {
                 Icon(Icons.Filled.Menu, contentDescription = "Localized description")
             }
         }
@@ -75,68 +56,5 @@ fun BottomNavBar() {
         IconButton(onClick = { /* doSomething() */ }) {
             Icon(Icons.Filled.Favorite, contentDescription = "Localized description")
         }
-    }
-}
-
-@OptIn(ExperimentalMaterialApi::class)
-@Composable
-fun BottomNavigationDrawer() {
-    val (gesturesEnabled, toggleGesturesEnabled) = remember { mutableStateOf(true) }
-    val scope = rememberCoroutineScope()
-    Column {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .toggleable(
-                    value = gesturesEnabled,
-                    onValueChange = toggleGesturesEnabled
-                )
-        ) {
-            Checkbox(gesturesEnabled, null)
-            Text(text = if (gesturesEnabled) "Gestures Enabled" else "Gestures Disabled")
-        }
-
-        val drawerState = rememberBottomDrawerState(BottomDrawerValue.Closed)
-        BottomDrawer(
-            gesturesEnabled = gesturesEnabled,
-            drawerState = drawerState,
-            drawerContent = {
-                Button(
-                    modifier = Modifier
-                        .align(Alignment.CenterHorizontally)
-                        .padding(top = 16.dp),
-                    onClick = { scope.launch { drawerState.close() } },
-                    content = { Text("Close Drawer") }
-                )
-                LazyColumn {
-                    items(25) {
-                        ListItem(
-                            text = { Text("Item $it") },
-                            icon = {
-                                Icon(
-                                    Icons.Default.Favorite,
-                                    contentDescription = "Localized description"
-                                )
-                            }
-                        )
-                    }
-                }
-            },
-        ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                val openText = if (gesturesEnabled) "▲▲▲ Pull up ▲▲▲" else "Click the button!"
-                Text(text = if (drawerState.isClosed) openText else "▼▼▼ Drag down ▼▼▼")
-                Spacer(Modifier.height(20.dp))
-                Button(onClick = { scope.launch { drawerState.open() } }) {
-                    Text("Click to open")
-                }
-            }
-        }
-
     }
 }

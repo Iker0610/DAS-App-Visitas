b8f008318b8c87311dbbbad2d70ea21a95653067
Iker de la Iglesia <ikerprogrammer0610@gmail.com>
2022-03-23 16:44:44 +0100

    Added Navigation Rail
    // TODO: Solve Bug in Bottom bar

--- app/src/main/java/das/omegaterapia/visits/activities/main/MainActivity.kt
+++ app/src/main/java/das/omegaterapia/visits/activities/main/MainActivity.kt
@@ -4,45 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.*
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
-import androidx.compose.animation.scaleIn
-import androidx.compose.animation.scaleOut
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CutCornerShape
-import androidx.compose.material.BottomDrawer
-import androidx.compose.material.BottomDrawerValue
-import androidx.compose.material.Divider
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.FabPosition
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.rememberBottomDrawerState
-import androidx.compose.material.rememberScaffoldState
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
@@ -57,14 +32,12 @@ import dagger.hilt.android.AndroidEntryPoint
 import das.omegaterapia.visits.R
 import das.omegaterapia.visits.activities.main.composables.BottomNavBar
 import das.omegaterapia.visits.activities.main.composables.MainFloatingActionButton
-import das.omegaterapia.visits.activities.main.screens.AddVisitScreen
-import das.omegaterapia.visits.activities.main.screens.AllVisitsScreen
-import das.omegaterapia.visits.activities.main.screens.MainActivityScreens
-import das.omegaterapia.visits.activities.main.screens.TodaysVisitsScreen
-import das.omegaterapia.visits.activities.main.screens.VIPVisitsScreen
+import das.omegaterapia.visits.activities.main.screens.*
+import das.omegaterapia.visits.ui.components.generic.CenteredColumn
 import das.omegaterapia.visits.ui.components.generic.DrawerButton
 import das.omegaterapia.visits.ui.theme.OmegaterapiaTheme
 import das.omegaterapia.visits.utils.WindowSize
+import das.omegaterapia.visits.utils.WindowSizeFormat
 import das.omegaterapia.visits.utils.rememberWindowSizeClass
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -103,11 +76,33 @@ private fun MainActivityScreen(
 
     val scaffoldState = rememberScaffoldState()
 
-    var isScrolling by rememberSaveable { mutableStateOf(false) } // TODO
+    var isScrolling by rememberSaveable { mutableStateOf(false) }
+
+    //-------------------------------------------------------------------
+    // Navigation booleans
+    val enableNavigation = currentRoute?.destination?.route != MainActivityScreens.AddVisit.route
+    val enableBottomNavigation = enableNavigation && windowSize.width == WindowSizeFormat.Compact
+    val enableRailNavigation = enableNavigation && !enableBottomNavigation
+
+    val showBottomNavigation = enableBottomNavigation && !isScrolling
+    //--------------------------------------------------------------------
+
 
     val drawerState = rememberBottomDrawerState(BottomDrawerValue.Closed)
     val gesturesEnabled = drawerState.currentValue != BottomDrawerValue.Closed
 
+    LaunchedEffect(windowSize.width) {
+        if (windowSize.width != WindowSizeFormat.Compact) {
+            scope.launch { drawerState.close() }
+        }
+    }
+
+    @Composable
+    fun FAB() {
+        MainFloatingActionButton(
+            onClick = { navController.navigate(MainActivityScreens.AddVisit.route) },
+        )
+    }
 
     BottomDrawer(
         gesturesEnabled = gesturesEnabled,
@@ -116,7 +111,10 @@ private fun MainActivityScreen(
         drawerContent = {
             LazyColumn(Modifier.padding(top = 16.dp, bottom = 22.dp)) {
                 item {
-                    NavDrawerHeader(currentUser = visitViewModel.currentUser, Modifier.padding(horizontal = 16.dp)) {
+                    NavDrawerHeader(
+                        currentUser = visitViewModel.currentUser,
+                        Modifier.padding(horizontal = 16.dp)
+                    ) {
                         scope.launch { drawerState.close() }
                     }
                     Divider(
@@ -149,20 +147,18 @@ private fun MainActivityScreen(
             scaffoldState = scaffoldState,
             floatingActionButton = {
                 AnimatedVisibility(
-                    currentRoute?.destination?.route != MainActivityScreens.AddVisit.route && !isScrolling,
+                    showBottomNavigation,
                     enter = scaleIn(),
                     exit = scaleOut()
                 ) {
-                    MainFloatingActionButton(
-                        onClick = { navController.navigate(MainActivityScreens.AddVisit.route) },
-                    )
+                    FAB()
                 }
             },
             floatingActionButtonPosition = FabPosition.Center,
             isFloatingActionButtonDocked = true,
             bottomBar = {
                 AnimatedVisibility(
-                    currentRoute?.destination?.route != MainActivityScreens.AddVisit.route && !isScrolling,
+                    showBottomNavigation,
                     enter = slideInVertically(initialOffsetY = { 2 * it }),
                     exit = slideOutVertically(targetOffsetY = { 2 * it })
                 ) {
@@ -172,70 +168,111 @@ private fun MainActivityScreen(
                     )
                 }
             },
-
-            )
+        )
         { innerPadding ->
-            AnimatedNavHost(
-                navController = navController,
-                startDestination = MainActivityScreens.TodaysVisits.route
+            Row(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding),
+                verticalAlignment = Alignment.CenterVertically
             ) {
-
-                composable(route = MainActivityScreens.TodaysVisits.route) {
-                    TodaysVisitsScreen(
-                        visitViewModel = visitViewModel,
-                        onScrollStateChange = { isScrolling = it },
-                    )
-                }
-
-
-                composable(route = MainActivityScreens.AllVisits.route) {
-                    AllVisitsScreen(
-                        visitViewModel = visitViewModel,
-                        onScrollStateChange = { isScrolling = it },
-                    )
-                }
-
-
-                composable(route = MainActivityScreens.VIPs.route) {
-                    VIPVisitsScreen(
-                        visitViewModel = visitViewModel,
-                        onScrollStateChange = { isScrolling = it },
-                    )
-                }
-
-
-                composable(
-                    route = MainActivityScreens.AddVisit.route,
-                    enterTransition = {
-                        slideInVertically(
-                            initialOffsetY = { 2 * it },
-                            animationSpec = spring(
-                                dampingRatio = Spring.DampingRatioNoBouncy,
-                                stiffness = Spring.StiffnessVeryLow
-                            )
-                        )
-                    },
-                    exitTransition = {
-                        slideOutVertically(
-                            targetOffsetY = { 2 * it },
-                            animationSpec = spring(
-                                dampingRatio = Spring.DampingRatioNoBouncy,
-                                stiffness = Spring.StiffnessVeryLow
-                            )
-                        )
-                    },
-                ) {
-                    AddVisitScreen(
-                        modifier = Modifier.padding(innerPadding),
-                        addVisitCard = visitViewModel::addVisitCard,
-                        onSuccessfulSubmit = {
-                            scope.launch(Dispatchers.Main) {
-                                if (!navController.popBackStack()) {
-                                    navController.navigate(MainActivityScreens.AllVisits.route)
-                                }
+                if (enableRailNavigation) {
+                    NavigationRail(
+                        header = { Box(Modifier.padding(horizontal = 8.dp)) { FAB() } }
+                    ) {
+                        CenteredColumn(Modifier.weight(1f, true)) {
+                            MainActivityScreens.navigableScreens.forEach { screen ->
+                                NavigationRailItem(
+                                    icon = { Icon(screen.icon, contentDescription = screen.title) },
+                                    label = null,
+                                    selected = currentRoute?.destination?.route == screen.route,
+                                    onClick = {
+                                        navController.navigate(screen.route) {
+                                            popUpTo(navController.graph.startDestinationId) {
+                                                saveState = true
+                                            }
+                                            launchSingleTop = true
+                                            restoreState = true
+                                        }
+                                    }
+                                )
                             }
                         }
-                    )
+                        NavigationRailItem(
+                            icon = {
+                                Icon(
+                                    Icons.Filled.AccountCircle,
+                                    contentDescription = "User Settings"
+                                )
+                            },
+                            label = null,
+                            selected = false,
+                            onClick = { /*TODO*/ }
+                        )
+                    }
+                }
+
+                AnimatedNavHost(
+                    navController = navController,
+                    startDestination = MainActivityScreens.TodaysVisits.route
+                ) {
+
+                    composable(route = MainActivityScreens.TodaysVisits.route) {
+                        TodaysVisitsScreen(
+                            visitViewModel = visitViewModel,
+                            onScrollStateChange = { isScrolling = it },
+                        )
+                    }
+
+
+                    composable(route = MainActivityScreens.AllVisits.route) {
+                        AllVisitsScreen(
+                            visitViewModel = visitViewModel,
+                            onScrollStateChange = { isScrolling = it },
+                        )
+                    }
+
+
+                    composable(route = MainActivityScreens.VIPs.route) {
+                        VIPVisitsScreen(
+                            visitViewModel = visitViewModel,
+                            onScrollStateChange = { isScrolling = it },
+                        )
+                    }
+
+
+                    composable(
+                        route = MainActivityScreens.AddVisit.route,
+                        enterTransition = {
+                            slideInVertically(
+                                initialOffsetY = { 2 * it },
+                                animationSpec = spring(
+                                    dampingRatio = Spring.DampingRatioNoBouncy,
+                                    stiffness = Spring.StiffnessVeryLow
+                                )
+                            )
+                        },
+                        exitTransition = {
+                            slideOutVertically(
+                                targetOffsetY = { 2 * it },
+                                animationSpec = spring(
+                                    dampingRatio = Spring.DampingRatioNoBouncy,
+                                    stiffness = Spring.StiffnessVeryLow
+                                )
+                            )
+                        },
+                    ) {
+                        AddVisitScreen(
+                            addVisitCard = visitViewModel::addVisitCard,
+                            onSuccessfulSubmit = {
+                                scope.launch(Dispatchers.Main) {
+                                    if (!navController.popBackStack()) {
+                                        navController.navigate(MainActivityScreens.AllVisits.route)
+                                    }
+                                }
+                            }
+                        )
+                    }
                 }
             }
         }
@@ -244,7 +281,11 @@ private fun MainActivityScreen(
 
 
 @Composable
-private fun NavDrawerHeader(currentUser: String, modifier: Modifier = Modifier, onClose: () -> Unit) {
+private fun NavDrawerHeader(
+    currentUser: String,
+    modifier: Modifier = Modifier,
+    onClose: () -> Unit
+) {
     Row(
         modifier.fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically

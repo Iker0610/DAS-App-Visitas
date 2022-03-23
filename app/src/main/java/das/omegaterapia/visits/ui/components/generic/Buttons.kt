00fbe006ef2cb59177698ab239e439f6e236dcdd
iker0610 <ikerprogrammer0610@gmail.com>
2022-03-23 13:51:40 +0100

    Added Required Buttons for navigation

--- app/src/main/java/das/omegaterapia/visits/ui/components/generic/Buttons.kt
+++ app/src/main/java/das/omegaterapia/visits/ui/components/generic/Buttons.kt
@@ -1,18 +1,40 @@
 package das.omegaterapia.visits.ui.components.generic
 
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.ButtonColors
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.ButtonElevation
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.unit.dp
 import das.omegaterapia.visits.ui.theme.getButtonShape
 
 @Composable
@@ -43,3 +65,130 @@ fun FixedOutlinedButton(
         content = content
     )
 }
+
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+
+/*
+*  Code from Google's JetNews Application for Compose (it implements the *button styles* needed for navigation drawers)
+*/
+
+@Composable
+fun DrawerButton(
+    icon: ImageVector,
+    label: String,
+    isSelected: Boolean,
+    action: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val colors = MaterialTheme.colors
+    val textIconColor = if (isSelected) {
+        colors.primary
+    } else {
+        colors.onSurface.copy(alpha = 0.6f)
+    }
+    val backgroundColor = if (isSelected) {
+        colors.primary.copy(alpha = 0.12f)
+    } else {
+        Color.Transparent
+    }
+
+    val surfaceModifier = modifier
+        .padding(start = 8.dp, top = 8.dp, end = 8.dp)
+        .fillMaxWidth()
+    Surface(
+        modifier = surfaceModifier,
+        color = backgroundColor,
+        shape = MaterialTheme.shapes.small
+    ) {
+        TextButton(
+            onClick = action,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                NavigationIcon(
+                    icon = icon,
+                    isSelected = isSelected,
+                    contentDescription = null, // decorative
+                    tintColor = textIconColor
+                )
+                Spacer(Modifier.width(16.dp))
+                Text(
+                    text = label,
+                    style = MaterialTheme.typography.body2,
+                    color = textIconColor
+                )
+            }
+        }
+    }
+}
+
+
+@Composable
+fun NavigationIcon(
+    icon: ImageVector,
+    isSelected: Boolean,
+    modifier: Modifier = Modifier,
+    contentDescription: String? = null,
+    tintColor: Color? = null,
+) {
+    val imageAlpha = if (isSelected) {
+        1f
+    } else {
+        0.6f
+    }
+
+    val iconTintColor = tintColor ?: if (isSelected) {
+        MaterialTheme.colors.primary
+    } else {
+        MaterialTheme.colors.onSurface.copy(alpha = 0.6f)
+    }
+
+    Image(
+        modifier = modifier,
+        imageVector = icon,
+        contentDescription = contentDescription,
+        contentScale = ContentScale.Inside,
+        colorFilter = ColorFilter.tint(iconTintColor),
+        alpha = imageAlpha
+    )
+}
+
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun NavRailIcon(
+    icon: ImageVector,
+    contentDescription: String,
+    isSelected: Boolean,
+    action: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val backgroundColor by animateColorAsState(
+        if (isSelected) {
+            MaterialTheme.colors.primary.copy(alpha = 0.12f)
+        } else {
+            Color.Transparent
+        }
+    )
+
+    Surface(
+        color = backgroundColor,
+        onClick = action,
+        shape = CircleShape,
+        role = Role.Tab,
+        modifier = modifier.size(48.dp)
+    ) {
+        NavigationIcon(
+            icon = icon,
+            isSelected = isSelected,
+            contentDescription = contentDescription,
+            modifier = Modifier.size(32.dp)
+        )
+    }
+}
+//----------------------------------------------------------------------------------------------------------------------------------------------------

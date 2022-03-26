3d7265eb8b248c6fc9ea188df38e30358acfc3a1
iker0610 <ikerprogrammer0610@gmail.com>
2022-03-26 21:10:29 +0100

    Application documented

--- app/src/main/java/das/omegaterapia/visits/OmegaterapiaVisitsApp.kt
+++ app/src/main/java/das/omegaterapia/visits/OmegaterapiaVisitsApp.kt
@@ -7,6 +7,20 @@ import android.app.NotificationManager
 import dagger.hilt.android.HiltAndroidApp
 
 
+/*******************************************************************************
+ ****                        Custom Aplication Class                        ****
+ *******************************************************************************/
+
+/*
+
+This class is needed for Hilt Framework.
+
+I also initialize notification channels here so they are not created each time we send a notification.
+
+Avoiding code  repetition
+
+*/
+
 @HiltAndroidApp
 class OmegaterapiaVisitsApp : Application() {
 
@@ -15,9 +29,9 @@ class OmegaterapiaVisitsApp : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        /*------------------------------
-        | Create Notification Channels |
-        ------------------------------*/
+        /*------------------------------------------------
+        |          Create Notification Channels          |
+        ------------------------------------------------*/
 
         // Create the Authentication Notification Channel
         val name = getString(R.string.auth_channel_name)
@@ -31,6 +45,18 @@ class OmegaterapiaVisitsApp : Application() {
         notificationManager.createNotificationChannel(mChannel)
     }
 }
+
+
+/*******************************************************************************
+ ****                    Enum Class for Notification IDs                    ****
+ *******************************************************************************/
+
+/*
+Class used to centralize and have better control over application's notification IDs.
+It uses an enum class what gives better readability over the code, and avoids ID mistakes
+
+(In this app we only had one notification, but it's a good practice and eases future expansions and technical debt)
+*/
 
 enum class NotificationID(val id: Int) {
     USER_CREATED(0)
